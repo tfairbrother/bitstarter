e9948167d7fc1fdde91a33bf1f08bfdc9d08ffd4
Thomas Fairbrother <tfairbrother.gmail.com>
2013-07-10 15:13:18 +0000

    Modify web.js for index.html

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
+
 var express = require('express');
+
+var fs = require('fs');
+
+var buf = new Buffer ('Default');
+buf = fs.readFileSync('index.html');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buf.toString("utf-8"));
 });
 
 var port = process.env.PORT || 5000;
